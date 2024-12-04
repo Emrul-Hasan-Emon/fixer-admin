315dbb39763a164e1a1272e984c15fe0b1c22839
Emrul Hasan Emon <emrulhasanemon12@gmail.com>
2024-12-04 21:30:56 +0600

    Components are declared in App Module

--- fixer-admin/src/app/app.module.ts
+++ fixer-admin/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NewFixersComponent } from './components/new-fixers/new-fixers.component';
+import { RegisteredFixersComponent } from './components/registered-fixers/registered-fixers.component';
+import { ClientsComponent } from './components/clients/clients.component';
+import { TaskComponent } from './components/task/task.component';
+import { ProposalComponent } from './components/proposal/proposal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewFixersComponent,
+    RegisteredFixersComponent,
+    ClientsComponent,
+    TaskComponent,
+    ProposalComponent
   ],
   imports: [
     BrowserModule,
