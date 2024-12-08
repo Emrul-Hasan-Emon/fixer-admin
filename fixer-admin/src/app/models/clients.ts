dd30658cce290bc6a73927b6faf66fd7c32be031
Emrul Hasan Emon <emrulhasanemon12@gmail.com>
2024-12-08 22:54:40 +0600

    Model: Client model is created

---
+++ fixer-admin/src/app/models/clients.ts
@@ -0,0 +1,11 @@
+export interface Client {
+    client_id: string;
+    phone_number: string;
+    first_name: string;
+    last_name: string;
+    dob: string;
+    city: string;
+    gender: string;
+    created_at: string;
+  }
+  
