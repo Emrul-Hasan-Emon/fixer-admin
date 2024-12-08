0cd5ffe3c7066a3cf5e46961f8f937923efda27c
Emrul Hasan Emon <emrulhasanemon12@gmail.com>
2024-12-08 17:38:55 +0600

    Functionality: Login Functionality is integrated

--- fixer-admin/src/app/components/login/login.component.ts
+++ fixer-admin/src/app/components/login/login.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  username: string = '';
+  password: string = '';
+  rememberMe: boolean = false;
+  showPassword: boolean = false;
 
+  togglePassword() {  // Add this method
+    this.showPassword = !this.showPassword;
+  }
+  
+  onSubmit() {
+    // Handle login logic here
+    console.log({
+      username: this.username,
+      password: this.password,
+      rememberMe: this.rememberMe
+    });
+  }
 }
