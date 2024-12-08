caf8f823ee9e7d9a988f9c27a3addd38861e814a
Emrul Hasan Emon <emrulhasanemon12@gmail.com>
2024-12-08 22:54:59 +0600

    Service: Client Service is updated for getting all clients information

--- fixer-admin/src/app/services/client/client.service.ts
+++ fixer-admin/src/app/services/client/client.service.ts
@@ -1,9 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { UrlService } from '../url.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClientService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  public getAllClients() {
+    return this.http.get(this.formURL());
+  }
+
+  private formURL(): string {
+    return "/admin/client"
+  }
 }
