6e7e620dc6ad3852c7117eef73ea035e10fa4f18
Emrul Hasan Emon <emrulhasanemon12@gmail.com>
2024-12-08 22:55:31 +0600

    HTML Template, CSS and Component class is updated for showing Clients

--- fixer-admin/src/app/components/clients/clients.component.ts
+++ fixer-admin/src/app/components/clients/clients.component.ts
@@ -1,19 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Client } from 'src/app/models/clients';
+import { ClientService } from 'src/app/services/client/client.service';
 
 @Component({
   selector: 'app-clients',
   templateUrl: './clients.component.html',
   styleUrls: ['./clients.component.css']
 })
-export class ClientsComponent {
-  clients = [
-    { id: 1, name: 'John Doe', gender: 'Male' },
-    { id: 2, name: 'Jane Smith', gender: 'Female' },
-    { id: 3, name: 'Alice Johnson', gender: 'Female' },
-    // ... more clients
-  ];
+export class ClientsComponent implements OnInit {
+  constructor(private clientService: ClientService) {}
+  clients: Client[] = [];
 
-  seeDetails(clientId: number) {
+  ngOnInit(): void {
+    this.fetchAllClients();
+  }
+
+  private fetchAllClients() {
+    this.clientService.getAllClients().subscribe(
+      (clients: any) => {
+        this.setClientsData(clients);
+      },
+      (err) => {
+        console.error("An unexpected occured");
+        // alert('Error occured while getting all client information');        
+      }
+    )
+  }
+
+  getFullName(client: Client): string {
+    return client.first_name + ' ' + client.last_name;
+  }
+
+  private setClientsData(clients: any) {
+    if (clients.message) {
+      if (clients.message == 'Client Information found') {
+        this.clients = clients.data;
+        console.log('');
+        console.log('Clients: ', this.clients);
+      } else {
+        this.clients = [];
+      }
+    }
+  }
+
+  seeDetails(clientId: Client) {
     // Logic to show client details later on
     console.log('See details for Client ID:', clientId);
   }
