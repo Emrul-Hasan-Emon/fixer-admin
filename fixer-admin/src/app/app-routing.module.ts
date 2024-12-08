80a4ffcd234cf80b011d0f8e3af757c7b54f7836
Emrul Hasan Emon <emrulhasanemon12@gmail.com>
2024-12-08 22:52:59 +0600

    Routing is set for welcome component

--- fixer-admin/src/app/app-routing.module.ts
+++ fixer-admin/src/app/app-routing.module.ts
@@ -15,6 +15,9 @@ const routes: Routes = [
     path: "", component: WelcomeComponent
   },
   {
+    path: "welcome", component: WelcomeComponent
+  },
+  {
     path: "login", component: LoginComponent
   },
   {
