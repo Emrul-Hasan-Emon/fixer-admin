9b4f47c70212f9f647bcee04f5b91e7173e6db6f
Emrul Hasan Emon <emrulhasanemon12@gmail.com>
2024-12-15 18:31:37 +0600

    Service: Client Service is updated

--- fixer-admin/src/app/services/client/client.service.ts
+++ fixer-admin/src/app/services/client/client.service.ts
@@ -1,18 +1,28 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { UrlService } from '../url.service';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { Client } from 'src/app/models/clients';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClientService {
-
+  public cartSource =  new BehaviorSubject<any>(null);
   constructor(private http: HttpClient) { }
 
   public getAllClients() {
     return this.http.get(this.formURL());
   }
 
+  public publishClientInformation(client: Client) {
+    this.cartSource.next(client);
+  }
+
+  public getPublishedClient(): Observable<Client> {
+    return this.cartSource.asObservable();
+  }
+  
   private formURL(): string {
     return "/admin/client"
   }
