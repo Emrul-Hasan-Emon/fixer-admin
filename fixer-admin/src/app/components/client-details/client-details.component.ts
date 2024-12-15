c8c735fff868c5cfc04844e111e430b137b52065
Emrul Hasan Emon <emrulhasanemon12@gmail.com>
2024-12-15 18:32:51 +0600

    Component Class: Component Class is updated for Client Details Component

--- fixer-admin/src/app/components/client-details/client-details.component.ts
+++ fixer-admin/src/app/components/client-details/client-details.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Client } from 'src/app/models/clients';
+import { Task, TaskResponse } from 'src/app/models/task';
+import { ClientService } from 'src/app/services/client/client.service';
+import { TaskService } from 'src/app/services/task/task.service';
 
 @Component({
   selector: 'app-client-details',
@@ -7,35 +11,54 @@ import { ActivatedRoute, Router } from '@angular/router';
   styleUrls: ['./client-details.component.css']
 })
 export class ClientDetailsComponent {
-  client: any;
-  p = [1, 2, 3]
-  constructor(private route: ActivatedRoute, private router: Router) {}
+  client: Client | undefined = undefined;
+  clientTasks: Task[] = [];
+  
+  constructor(
+    private router: Router,
+    private clientService: ClientService,
+    private taskService: TaskService
+  ) {}
 
   ngOnInit(): void {
-    const clientId = this.route.snapshot.paramMap.get('id');
-    this.fetchClientDetails(clientId);
-  }
-
-  fetchClientDetails(clientId: string | null) {
-    // Simulating API call with hard-coded data
-    if (clientId === '674cac225c13112c2b72701d') {
-      this.client = {
-        client_id: '674cac225c13112c2b72701d',
-        phone_number: '01700000002',
-        first_name: 'Tanvir',
-        last_name: 'Ahmed',
-        dob: '1996-08-14',
-        city: 'Karachi',
-        gender: 'Male',
-        created_at: '2024-12-02 00:34:10'
-      };
-    } else {
-      // Handle case where client ID is not found
-      this.client = null;
-    }
+      this.fetchClientDetails();
   }
 
   goBack() {
-    this.router.navigate(['/clients']); // Navigate back to the Clients page
+    this.router.navigate(['/clients']);
+  }
+
+  getFullName(client: Client): string {
+    return client.first_name + ' ' + client.last_name;
+  }
+
+  private fetchClientDetails() {
+    this.clientService.getPublishedClient().subscribe(
+      (client: Client) => {
+        this.client = client;
+        this.fetchAllTasksForTheClient(this.client.client_id); 
+      },
+      (error) => {
+        console.log(error);
+        alert('An unexpected error');
+      }
+    )
+  }
+
+  private fetchAllTasksForTheClient(clientId: string) {
+    this.taskService.getAllTasksForAClient(clientId).subscribe(
+      (tasks: TaskResponse) => {
+        this.clientTasks = tasks.data
+      },
+      (error) => {
+        console.log(error);
+        alert('An unexpected error occured while fetching all tasks');
+      }
+    )
+  }
+
+  public seeTaskDetails(task: Task) {
+    this.taskService.publishTaskDetails(task);
+    this.router.navigate(['task-details'])
   }
 }
