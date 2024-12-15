d22c82c230e714cdc385890f8b97a371c0702a72
Emrul Hasan Emon <emrulhasanemon12@gmail.com>
2024-12-15 18:31:24 +0600

    Service: Task Service is updated

--- fixer-admin/src/app/services/task/task.service.ts
+++ fixer-admin/src/app/services/task/task.service.ts
@@ -1,9 +1,37 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { Task, TaskResponse } from 'src/app/models/task';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
+    public taskDetails =  new BehaviorSubject<any>(null);
+  constructor(
+    private http: HttpClient
+  ) { }
 
-  constructor() { }
+  public getAllTasksForAClient(clientId: string): Observable<TaskResponse> {
+    const payload = {
+      client_id: clientId
+    };
+    return this.http.post<TaskResponse>(this.getURLForClientTasks(), payload);
+  }
+
+  public getAllTasks(): Observable<TaskResponse> {
+    return this.http.get<TaskResponse>('/admin/task/all-tasks-details');
+  }
+
+  public publishTaskDetails(taskDetails: Task) {
+    this.taskDetails.next(taskDetails);
+  }
+
+  public getPublishedTaskDetails(): Observable<Task> {
+    return this.taskDetails.asObservable();
+  }
+
+  private getURLForClientTasks() {
+    return '/admin/task/client-tasks'
+  }
 }
