a6c17aeba726538e0a02306c5d92391f5995e3f0
Emrul Hasan Emon <emrulhasanemon12@gmail.com>
2024-12-16 13:55:19 +0600

    Service: Proposal Service is updated with functionalities

--- fixer-admin/src/app/services/proposal/proposal.service.ts
+++ fixer-admin/src/app/services/proposal/proposal.service.ts
@@ -1,9 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ProposalResponse } from 'src/app/models/proposal';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProposalService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  public getProposalsForAFixer(fixerId: string): Observable<ProposalResponse> {
+    const payload = {
+      fixer_id: fixerId
+    }
+    return this.http.post<ProposalResponse>('/admin/get-proposals', payload);
+  }
 }
