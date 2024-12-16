deac8c4be33af2bc3172898783508ec9034f765a
Emrul Hasan Emon <emrulhasanemon12@gmail.com>
2024-12-16 13:55:00 +0600

    Service: Fixer Service is updated with functionalities

--- fixer-admin/src/app/services/fixer/fixer.service.ts
+++ fixer-admin/src/app/services/fixer/fixer.service.ts
@@ -1,9 +1,31 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { Fixer, FixerResponse } from 'src/app/models/fixer';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FixerService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  private newFixerURL: string = '/admin/new-fixers';
+  private oldFixerURL: string = '/admin/registered-fixers';
+  private fixerDetails = new BehaviorSubject<any>(null);
+
+  public getNewlyRegisteredFixers(): Observable<FixerResponse> {
+    return this.http.get<FixerResponse>(this.newFixerURL);
+  }
+
+  public getRegisteredFixers(): Observable<FixerResponse> {
+    return this.http.get<FixerResponse>(this.oldFixerURL);
+  }
+
+  public publishFixerDetails(fixer: Fixer) {
+    this.fixerDetails.next(fixer);
+  }
+
+  public getPublishedFixerDetails() {
+    return this.fixerDetails.asObservable();
+  }
 }
