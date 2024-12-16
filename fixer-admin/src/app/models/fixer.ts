db95ad0916ba3911ecdeedf73d55ed13db9cc102
Emrul Hasan Emon <emrulhasanemon12@gmail.com>
2024-12-16 13:54:29 +0600

    Model: Fixer model is created

---
+++ fixer-admin/src/app/models/fixer.ts
@@ -0,0 +1,22 @@
+export interface Fixer {
+  fixer_id: string;
+  phone_number: string;
+  first_name: string;
+  last_name: string;
+  dob: string;
+  city: string;
+  gender: string;
+  created_at: string;
+  category_id: string;
+  cnic_front: string[];
+  cnic_back: string[];
+  profile_image: string[];
+  certificates_image: string[];
+}
+
+export interface FixerResponse {
+  data: Fixer[];
+  message: string;
+  role: string;
+  status: number;
+}
