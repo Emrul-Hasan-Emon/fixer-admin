b49c3529ac9093356a42c2a08d557d48acbc4d36
Emrul Hasan Emon <emrulhasanemon12@gmail.com>
2025-01-13 21:20:22 +0600

    Service: Slider Image Service is updated

--- fixer-admin/src/app/services/slider/slider-image.service.ts
+++ fixer-admin/src/app/services/slider/slider-image.service.ts
@@ -1,9 +1,23 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { SliderImageResponse } from 'src/app/models/slider_image';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SliderImageService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  public getAllSliderImages(): Observable<SliderImageResponse> {
+    return this.http.get<SliderImageResponse>('/admin/slider-image');
+  }
+
+  public addNewSliderImage(slider_image: string): Observable<any> {
+    const payload = {
+      slider_image: slider_image
+    };
+    return this.http.post('/admin/slider-image', payload);
+  }
 }
