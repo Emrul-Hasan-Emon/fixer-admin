b4d6c5c55c5f8852cb0a52ec0a5cda5ac559f5bf
Emrul Hasan Emon <emrulhasanemon12@gmail.com>
2025-01-13 21:57:03 +0600

    Component: Location Add Component is modified

--- fixer-admin/src/app/components/location-add/location-add.component.ts
+++ fixer-admin/src/app/components/location-add/location-add.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { LocationService } from 'src/app/services/location/location.service';
 
 @Component({
   selector: 'app-location-add',
@@ -6,5 +8,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./location-add.component.css']
 })
 export class LocationAddComponent {
+  location: string = '';
 
+  constructor (
+    private locationService: LocationService,
+    private router: Router
+  ) {}
+
+  public addNewLocation() {
+    if (this.location.length !== 0) {
+      this.locationService.addNewLocations(this.location).subscribe(
+        (resp: any) => {
+          this.router.navigate(['location']);
+        }
+      )
+    } else {
+      alert('Location filed is empty')
+    }
+  }
 }
