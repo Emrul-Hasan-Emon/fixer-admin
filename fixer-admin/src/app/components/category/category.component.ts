c8ba62dfa00dbc23b1fd3ab90ff176820aa86dbe
Emrul Hasan Emon <emrulhasanemon12@gmail.com>
2025-01-13 21:30:23 +0600

    Component: Category Component is modified

--- fixer-admin/src/app/components/category/category.component.ts
+++ fixer-admin/src/app/components/category/category.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Category, CategoryResponse } from 'src/app/models/category';
+import { CategoryService } from 'src/app/services/category/category.service';
 
 @Component({
   selector: 'app-category',
   templateUrl: './category.component.html',
   styleUrls: ['./category.component.css']
 })
-export class CategoryComponent {
+export class CategoryComponent implements OnInit {
+  categories: Category[] = [];
 
+  constructor(
+    private categoryService: CategoryService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.fetchAllCategories();
+  }
+
+  private fetchAllCategories() {
+    this.categoryService.getAllCategories().subscribe(
+      (resp: CategoryResponse) => {
+        if (resp.data) {
+          this.categories = resp.data;
+        } else {
+          this.categories = [];
+        }
+      },
+      (error) => {
+        console.log(error);
+        alert('An error occured while fetching all categories');
+      }
+    )
+  }
+
+  public addNewCategory() {
+    this.router.navigate(['category-add']);
+  }
 }
