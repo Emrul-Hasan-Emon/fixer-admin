0270a90de3a27755cd3ba9f50b5cf13a99d1edb8
Emrul Hasan Emon <emrulhasanemon12@gmail.com>
2025-01-13 21:20:44 +0600

    Service: Category Service is updated

--- fixer-admin/src/app/services/category/category.service.ts
+++ fixer-admin/src/app/services/category/category.service.ts
@@ -1,9 +1,23 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { CategoryResponse } from 'src/app/models/category';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoryService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  public getAllCategories(): Observable<CategoryResponse> {
+    return this.http.get<CategoryResponse>('/admin/category');
+  }
+
+  public addNewCategory(category: string): Observable<any> {
+    const payload = {
+      category: category
+    };
+    return this.http.post('/admin/category', payload);
+  }
 }
