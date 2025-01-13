4dd4b152be102f088915db6ddc6a6aa0c4a9fe76
Emrul Hasan Emon <emrulhasanemon12@gmail.com>
2025-01-13 21:37:44 +0600

    Component: Location Component is modified

--- fixer-admin/src/app/components/location/location.component.ts
+++ fixer-admin/src/app/components/location/location.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Location, LocationResponse } from 'src/app/models/location';
+import { LocationService } from 'src/app/services/location/location.service';
 
 @Component({
   selector: 'app-location',
   templateUrl: './location.component.html',
   styleUrls: ['./location.component.css']
 })
-export class LocationComponent {
+export class LocationComponent implements OnInit {
+  locations: Location[] = [];
 
+  constructor(
+    private locationService: LocationService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.fetchAllLocations();
+  }
+
+  private fetchAllLocations() {
+    this.locationService.getAllLocations().subscribe(
+      (resp: LocationResponse) => {
+        if (resp.data) {
+          this.locations = resp.data;
+        } else {
+          this.locations = [];
+        }
+      },
+      (error) => {
+        console.log(error);
+        alert('An error occured while getting locations');
+      }
+    )
+  }
+
+  public addNewLocation() {
+    this.router.navigate(['location-add']);
+  }
 }
