e59929587be95733a7ca5e901a23cb260c10622b
Emrul Hasan Emon <emrulhasanemon12@gmail.com>
2025-01-13 21:56:25 +0600

    Component: Category Add Component is added

--- fixer-admin/src/app/components/category-add/category-add.component.ts
+++ fixer-admin/src/app/components/category-add/category-add.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { CategoryService } from 'src/app/services/category/category.service';
 
 @Component({
   selector: 'app-category-add',
@@ -6,5 +8,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./category-add.component.css']
 })
 export class CategoryAddComponent {
+  category: string = '';
 
+  constructor(
+    private categoryService: CategoryService,
+    private router: Router
+  ) {}
+
+  public addNewImage() {
+    if (this.category.length !== 0) {
+      this.categoryService.addNewCategory(this.category).subscribe(
+        (resp: any) => {
+          this.router.navigate(['category']);
+        }
+      )
+    } else {
+      alert('Category filed is empty')
+    }
+  }
 }
