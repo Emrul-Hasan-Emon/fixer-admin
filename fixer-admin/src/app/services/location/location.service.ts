2b067d4ff2c9dff916291433cf0adce22cc2e90c
Emrul Hasan Emon <emrulhasanemon12@gmail.com>
2025-01-13 21:20:32 +0600

    Service: Location Service is updated

--- fixer-admin/src/app/services/location/location.service.ts
+++ fixer-admin/src/app/services/location/location.service.ts
@@ -1,9 +1,23 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { LocationResponse } from 'src/app/models/location';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LocationService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  public getAllLocations(): Observable<LocationResponse> {
+    return this.http.get<LocationResponse>('/admin/location');
+  }
+
+  public addNewLocations(location: string): Observable<any> {
+    const payload = {
+      location: location
+    };
+    return this.http.post('/admin/location', payload);
+  }
 }
