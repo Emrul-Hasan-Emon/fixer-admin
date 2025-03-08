886ec5583de0e1a39da138ad4947a7ea2214de3e
Emrul Hasan Emon <emrulhasanemon12@gmail.com>
2025-03-09 01:18:09 +0600

    model: Payment model is created

---
+++ fixer-admin/src/app/models/payment.ts
@@ -0,0 +1,9 @@
+export interface Payment {
+    payment_id: string;
+    ppAmount: string;
+    ppDescription: string;
+    pp_TxnRefNo: string;
+    pp_MerchantID: string;
+    pp_Password: string;
+    pp_SecureHash: string;
+}
